1085d4ece82a9a3d0feb230453a5811f31d07a37
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-11-25 17:20:33 -0400

    Cabecerahtml

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -1,3 +1,16 @@
+<?php
+$Nivel=$_SESSION['Nivel'];
+include_once("class/config.php");
+$config=new config;
+
+$Titulo=$config->mostrarConfig("Titulo",1);
+$Lema=$config->mostrarConfig("Lema",1);
+include_once("class/menu.php");
+$menu=new menu;
+include_once("class/submenu.php");
+$submenu=new submenu;
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,17 +20,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Page title -->
-    <title>HOMER | WebApp admin theme</title>
+    <title><?php echo $Titulo?></title>
 
-    <link rel="shortcut icon" type="image/ico" href="favicon_1.ico" />
+    <link rel="shortcut icon" type="image/ico" href="<?php echo $folder?>favicon_1.ico" />
 
     <!-- Vendor styles -->
-    <link rel="stylesheet" href="css/core/font-awesome.css" />
-    <link rel="stylesheet" href="css/core/metisMenu.css" />
-    <link rel="stylesheet" href="css/core/animate.css" />
-    <link rel="stylesheet" href="css/core/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/font-awesome.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/metisMenu.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/animate.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/bootstrap/css/bootstrap.css" />
 
     <!-- App styles -->
-    <link rel="stylesheet" href="css/core/pe-icon-7-stroke.css" />
-    <link rel="stylesheet" href="css/core/helper.css" />
-    <link rel="stylesheet" href="css/core/style.css">
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/pe-icon-7-stroke.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/helper.css" />
+    <link rel="stylesheet" href="<?php echo $folder?>css/core/style.css">

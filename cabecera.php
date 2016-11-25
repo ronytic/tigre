9d6e5ef03d1c56d1e8bb8afa7ea7685e9f3a037a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-11-25 17:20:29 -0400

    Cabecera

--- cabecera.php
+++ cabecera.php
@@ -13,13 +13,13 @@
     </div>
     <div id="logo" class="light-version">
         <span>
-            Homer Theme
+            <?php echo $Titulo;?>
         </span>
     </div>
     <nav role="navigation">
         <div class="header-link hide-menu"><i class="fa fa-bars"></i></div>
         <div class="small-logo">
-            <span class="text-primary">HOMER APP</span>
+            <span class="text-primary"><small><?php echo $Titulo;?></small></span>
         </div>
         <form role="search" class="navbar-form-custom" method="post" action="#">
             <div class="form-group">
@@ -33,20 +33,22 @@
             <div class="collapse mobile-navbar" id="mobile-collapse">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a class="" href="login.html">Login</a>
+                        <a class="" href="<?php echo $folder?>">Inicio</a>
                     </li>
+                    
                     <li>
-                        <a class="" href="login.html">Logout</a>
+                        <a class="" href="<?php echo $folder?>usuario/contrasena/">Modificar Contraseña</a>
                     </li>
+                    
                     <li>
-                        <a class="" href="profile.html">Profile</a>
+                        <a class="" href="<?php echo $folder?>login/logout.php">Salir del Sistema</a>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="navbar-right">
             <ul class="nav navbar-nav no-borders">
-                <li class="dropdown">
+                <!--<li class="dropdown">
                     <a class="dropdown-toggle" href="#" data-toggle="dropdown">
                         <i class="pe-7s-speaker"></i>
                     </a>
@@ -157,9 +159,9 @@
                     <a href="#" id="sidebar" class="right-sidebar-toggle">
                         <i class="pe-7s-upload pe-7s-news-paper"></i>
                     </a>
-                </li>
+                </li>-->
                 <li class="dropdown">
-                    <a href="login.html">
+                    <a href="<?php echo $folder?>login/logout.php" title="Salir del Sistema">
                         <i class="pe-7s-upload pe-rotate-90"></i>
                     </a>
                 </li>
@@ -205,123 +207,22 @@
 
         <ul class="nav" id="side-menu">
             <li class="active">
-                <a href="index-2.html"> <span class="nav-label">Dashboard</span> <span class="label label-success pull-right">v.1</span> </a>
+                <a href="<?php echo $folder;?>"> <span class="nav-label">Inicio</span> <span class="label label-success pull-right"></span> </a>
             </li>
-            <li>
-                <a href="analytics.html"> <span class="nav-label">Analytics</span><span class="label label-warning pull-right">NEW</span> </a>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Interface</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="panels.html">Panels design</a></li>
-                    <li><a href="typography.html">Typography</a></li>
-                    <li><a href="buttons.html">Colors &amp; Buttons</a></li>
-                    <li><a href="components.html">Components</a></li>
-                    <li><a href="alerts.html">Alerts</a></li>
-                    <li><a href="modals.html">Modals</a></li>
-                    <li><a href="loading_buttons.html">Loading buttons</a></li>
-                    <li><a href="draggable.html">Draggable panels</a></li>
-                    <li><a href="code_editor.html">Code editor</a></li>
-                    <li><a href="email_template.html">Email template</a></li>
-                    <li><a href="nestable_list.html">List</a></li>
-                    <li><a href="tour.html">Tour</a></li>
-                    <li><a href="icons.html">Icons library</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">App views</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="contacts.html">Contacts</a></li>
-                    <li><a href="projects.html">Projects</a></li>
-                    <li><a href="project.html">Project detail</a></li>
-                    <li><a href="app_plans.html">App plans</a></li>
-                    <li><a href="social_board.html">Social board</a></li>
-                    <li><a href="faq.html">FAQ</a></li>
-                    <li><a href="timeline.html">Timeline</a></li>
-                    <li><a href="notes.html">Notes</a></li>
-                    <li><a href="profile.html">Profile</a></li>
-                    <li><a href="mailbox.html">Mailbox</a></li>
-                    <li><a href="mailbox_compose.html">Email compose</a></li>
-                    <li><a href="mailbox_view.html">Email view</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="blog_details.html">Blog article</a></li>
-                    <li><a href="forum.html">Forum</a></li>
-                    <li><a href="forum_details.html">Forum details</a></li>
-                    <li><a href="gallery.html">Gallery</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
-                    <li><a href="invoice.html">Invoice</a></li>
-                    <li><a href="file_manager.html">File manager</a></li>
-                    <li><a href="chat_view.html">Chat view</a></li>
-                    <li><a href="search.html">Search view</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Charts</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="chartjs.html">ChartJs</a></li>
-                    <li><a href="flot.html">Flot charts</a></li>
-                    <li><a href="inline.html">Inline graphs</a></li>
-                    <li><a href="chartist.html">Chartist</a></li>
-                    <li><a href="c3.html">C3 Charts</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Box transitions</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="overview.html"><span class="label label-success pull-right">Start</span> Overview </a>  </li>
-                    <li><a href="transition_two.html">Columns from up</a></li>
-                    <li><a href="transition_one.html">Columns custom</a></li>
-                    <li><a href="transition_three.html">Panels zoom</a></li>
-                    <li><a href="transition_four.html">Rows from down</a></li>
-                    <li><a href="transition_five.html">Rows from right</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Common views</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="register.html">Register</a></li>
-                    <li><a href="error_one.html">Error 404</a></li>
-                    <li><a href="error_two.html">Error 505</a></li>
-                    <li><a href="lock.html">Lock screen</a></li>
-                    <li><a href="password_recovery.html">Passwor recovery</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Tables</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="tables_design.html">Tables design</a></li>
-                    <li><a href="datatables.html">Data tables</a></li>
-                    <li><a href="footable.html">Foo Table</a></li>
-
-                </ul>
-            </li>
-            <li>
-                <a href="widgets.html"> <span class="nav-label">Widgets</span> <span class="label label-success pull-right">Special</span></a>
-            </li>
-            <li>
-                <a href="#"><span class="nav-label">Forms</span><span class="fa arrow"></span> </a>
-                <ul class="nav nav-second-level">
-                    <li><a href="forms_elements.html">Forms elements</a></li>
-                    <li><a href="forms_extended.html">Forms extended</a></li>
-                    <li><a href="text_editor.html">Text editor</a></li>
-                    <li><a href="wizard.html">Wizard</a></li>
-                    <li><a href="validation.html">Validation</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="options.html"> <span class="nav-label">Layout options</span></a>
-            </li>
-            <li>
-                <a href="grid_system.html"> <span class="nav-label">Grid system</span></a>
-            </li>
-            <li>
-                <a href="landing_page.html"> <span class="nav-label">Landing page</span></a>
-            </li>
-            <li>
-                <a href="package.html"> <span class="nav-label">Package</span></a>
-            </li>
-
+            <?php foreach($menu->mostrar($Nivel) as $m){
+                ?>
+                <li>
+                    <a href="#"><span class="nav-label"><?php echo $m['Nombre']?></span><span class="fa arrow"></span> </a>
+                    <ul class="nav nav-second-level">
+                    <?php foreach($submenu->mostrar($Nivel,$m['CodMenu']) as $sm){
+                        ?>
+                        <li><a href="<?php echo $m['Url']?><?php echo $sm['Url']?>"><?php echo $sm['Nombre']?></a></li>
+                        <?php    
+                    }?>
+                    </ul>
+                </li>
+                <?php    
+            }?>
         </ul>
     </div>
 </aside>

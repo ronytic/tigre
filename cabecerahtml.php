fe9693b44197877d5afd70c49f270fdec8a045da
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-11-25 15:14:40 -0400

    Cabecerahtml

---
+++ cabecerahtml.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Page title -->
+    <title>HOMER | WebApp admin theme</title>
+
+    <link rel="shortcut icon" type="image/ico" href="favicon_1.ico" />
+
+    <!-- Vendor styles -->
+    <link rel="stylesheet" href="css/core/font-awesome.css" />
+    <link rel="stylesheet" href="css/core/metisMenu.css" />
+    <link rel="stylesheet" href="css/core/animate.css" />
+    <link rel="stylesheet" href="css/core/bootstrap/css/bootstrap.css" />
+
+    <!-- App styles -->
+    <link rel="stylesheet" href="css/core/pe-icon-7-stroke.css" />
+    <link rel="stylesheet" href="css/core/helper.css" />
+    <link rel="stylesheet" href="css/core/style.css">

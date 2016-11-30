fc28658b61d520a04a888081aa3b2575d76852b6
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-11-30 18:02:04 -0400

    Modificado Inicio

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include_once("login/check.php");
 $folder="";
-
+$titulo="Página de Inicio";
+$subtitulo="";
 ?>
 <?php include_once("cabecerahtml.php");?>
 <?php include_once("cabecera.php");?>
@@ -10,55 +11,78 @@ $folder="";
         <div class="panel-heading">
             <div class="panel-tools">
                 <a class="showhide"><i class="fa fa-chevron-up"></i></a>
-                <a class="closebox"><i class="fa fa-times"></i></a>
+                <!--<a class="closebox"><i class="fa fa-times"></i></a>-->
             </div>
-            Dashboard information and statistics
+            Página de Inicio
         </div>
         <div class="panel-body">
             <div class="row">
-                <div class="col-md-3 text-center">
-                    <div class="small">
-                        <i class="fa fa-bolt"></i> Page views
-                    </div>
-                    <div>
-                        <h1 class="font-extra-bold m-t-xl m-b-xs">
-                            226,802
-                        </h1>
-                        <small>Page views in last month</small>
-                    </div>
-                    <div class="small m-t-xl">
-                        <i class="fa fa-clock-o"></i> Data from January
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="text-center small">
-                        <i class="fa fa-laptop"></i> Active users in current month (December)
-                    </div>
-                    <div class="flot-chart" style="height: 160px">
-                        <div class="flot-chart-content" id="flot-line-chart"></div>
-                    </div>
-                </div>
-                <div class="col-md-3 text-center">
-                    <div class="small">
-                        <i class="fa fa-clock-o"></i> Active duration
-                    </div>
-                    <div>
-                        <h1 class="font-extra-bold m-t-xl m-b-xs">
-                            10 Months
-                        </h1>
-                        <small>And four weeks</small>
-                    </div>
-                    <div class="small m-t-xl">
-                        <i class="fa fa-clock-o"></i> Last active in 12.10.2015
+                <div class="col-lg-10 col-lg-offset-1">
+                
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                      </ol>
+                    
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <center>
+                          <img src="imagenes/inicio/1.jpg" alt="">
+                          </center>
+                          <div class="carousel-caption">
+                            ...
+                          </div>
+                        </div>
+                        <div class="item">
+                            <center>
+                          <img src="imagenes/inicio/2.jpg" alt="">
+                            </center>
+                          <div class="carousel-caption">
+                            ...
+                          </div>
+                        </div>
+                        <div class="item">
+                            <center>
+                          <img src="imagenes/inicio/3.jpg" alt="">
+                            </center>
+                          <div class="carousel-caption">
+                            ...
+                          </div>
+                        </div>
+                        <div class="item">
+                            <center>
+                          <img src="imagenes/inicio/4.jpg" alt="">
+                            </center>
+                          <div class="carousel-caption">
+                            ...
+                          </div>
+                        </div>
+                    
+                      </div>
+                    
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                      </a>
+                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Siguiente</span>
+                      </a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="panel-footer">
         <span class="pull-right">
-              You have two new messages from <a href="#">Monica Bolt</a>
+              
         </span>
-            Last update: 21.05.2015
+            
         </div>
     </div>
 </div>

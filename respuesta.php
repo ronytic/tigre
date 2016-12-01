3895e6437aa9ab2be1886c7e9c16500a0b29f086
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-01 11:19:33 -0400

    Respuesta

---
+++ respuesta.php
@@ -0,0 +1,26 @@
+<?php
+include_once("cabecerahtml.php");
+?>
+<?php include_once("cabecera.php");?>
+<div class="col-lg-12">
+    <ul>
+    <?php foreach($mensajes as $m){?>
+        <li><?php echo $m;?></li>
+    <?php }?>
+    </ul>
+    <?php if($nuevo==1){?>
+    <a href="index.php" class="btn btn-info">Nuevo Registro</a>
+    <?php }?>
+    <?php if($listar==1){?>
+    <a href="listar.php" class="btn btn-warning">Listado de Registro</a>
+    <?php }?>
+    <?php 
+        if(count($botones)>0){
+            foreach($botones as $k=>$b){
+                ?>
+                <a href="<?php echo $b[0];?>" class="btn btn-<?php echo $b[1];?>"><?php echo $k;?></a>
+                <?php
+            }
+        }?>
+</div>
+<?php include_once("pie.php");?>

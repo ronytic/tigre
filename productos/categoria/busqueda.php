79f9f4f9e8233464f65c52891afeecf8e2b94401
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-01 20:26:12 -0400

    Busqueda de Categoria

--- productos/categoria/busqueda.php
+++ productos/categoria/busqueda.php
@@ -3,6 +3,6 @@ include_once("../../login/check.php");
 extract($_POST);
 include_once("../../class/categoria.php");
 $categoria=new categoria;
-$cat=$categoria->mostrarTodoRegistro("nombre LIKE '$nombre%' and descripcion LIKE '$descripcion%'");
+$cat=$categoria->mostrarTodoRegistro("nombre LIKE '$nombre%' and descripcion LIKE '$descripcion%'",1,"nombre");
 listadotabla(array("nombre"=>"Nombre","descripcion"=>"Descripción"),$cat,1,"","modificar.php","eliminar.php");
 ?>

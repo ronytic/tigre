5dd34e1d033bdb7dfd5c07fd0df3224479733288
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-12-29 04:41:18 -0400

    Class Ivnentario

--- class/inventario.php
+++ class/inventario.php
@@ -2,9 +2,9 @@
 include_once("bd.php");
 class inventario extends bd{
 	var $tabla="inventario";
-    function cantidadStock($CodProducto){
-        $this->campos=array("sum(CantidadStock) as CantidadStock,sum(Cantidad) as Cantidad");
-        return $this->mostrarTodoRegistro("CodProducto=".$CodProducto);
+    function cantidadStock($CodProducto,$codsucursal){
+        $this->campos=array("sum(stock) as stock,sum(cantidadrecarga) as recarga");
+        return $this->mostrarTodoRegistro("CodProducto=".$CodProducto." and codsucursal=".$codsucursal);
     }
 	
 }
